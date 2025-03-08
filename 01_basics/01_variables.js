5f1b211ce2c5e6d77f3b7f585a5c33f485da34ae
Prajjwal Joshi <113706377+prajjwal333@users.noreply.github.com>
2025-03-08 11:03:10 +0000

    variables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,4 @@
+const name ="prajjwal" // not changeable
+let age =12 // if any variable need to change use let
+var ss = 1 // 1.prevent use of var  as it has function scope instead of block scope 
+          //  2.vairbale with the same name can be redeclared that will lead to overwrite of the values.
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+//Primitive Data types
+
+let name ="prajjwal" //String
+let age = 2 // Integer
+let isOld = false //Boolean
+
+
+// null -> empty/standalone value  typeof null -> object
+// undefined -> nothing defined typeof undefined -> undefined
+// symbol -> unqiue
+
+
+//Non-Primitive Data types
+
+//Object
+
+
+
+
+
+
